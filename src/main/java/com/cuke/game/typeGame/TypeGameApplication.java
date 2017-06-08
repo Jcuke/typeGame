989016871de2cb1d23023b97a1d1a30197057081
jcuke <937000078@qq.com>
2017-06-08 19:15:29 +0800

    Initial commit

---
+++ src/main/java/com/cuke/game/typeGame/TypeGameApplication.java
@@ -0,0 +1,12 @@
+package com.cuke.game.typeGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeGameApplication.class, args);
+	}
+}
